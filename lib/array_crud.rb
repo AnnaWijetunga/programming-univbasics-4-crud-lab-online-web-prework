123b84015cef5e2d78b2b6cf144bf2c682b307d3
Anna Wijetunga <annawijetunga@gmail.com>
2019-10-28 12:48:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,9 +2,7 @@ def create_an_empty_array
   []
 end
 
-def create_an_array["head", "shoulders", "knees", "toes"]
-  
-end
+create_an_array["head", "shoulders", "knees", "toes"]
 
 def add_element_to_end_of_array(array, element)
   
